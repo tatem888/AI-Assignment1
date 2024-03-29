33c0be564a286008e1545cd6bd3a2fdbe623aad3
Tate Manning <tatemanning@icloud.com>
2024-03-29 14:47:55 +1030

    wadaw

--- pathfinder.py
+++ pathfinder.py
@@ -1,6 +1,6 @@
 import numpy as np
 import sys
-import queue as Queue
+from queue import Queue
 
 #open map file, split lines into Map SIze, start point and end point tuple, and map data 2d array
 
@@ -64,19 +64,19 @@ def initStartNode(startPoint, mapData):
     return node
 #function to initilise fringe queue
 def initFringe(startNode):
-    q = Queue(maxsize=0)
+    q = Queue(maxsize = 50)
     q.put(startNode)
     return q
 
 #function to initilise closed set for searched coordinates
-def initClosed(mapSize, startNode):
+def initClosed(mapSize):
     arr = np.zeros(mapSize)
-    arr[Node.get_coordinates(startNode)] = 1
+    arr = np.pad(arr, (1,1), mode='constant',constant_values=(1,1))
     return arr
 
 #function to check if endpoint has been reached
 def endCheck(currentNode, endPoint):
-    a = currentNode.get_coordinates()
+    a = Node.get_coordinates(currentNode)
     if a == endPoint:
         return True
     else:
@@ -85,39 +85,45 @@ def endCheck(currentNode, endPoint):
 #function to generate nodes succeeding the state node and add them to fringe
 def expandFringe(stateNode, mapData, fringe, closed):
 
-    coords = stateNode.get_coordinates()
+    coords = Node.get_coordinates(stateNode)
     x,y = coords
 
-    up = (x, y-1)
-    down = (x, y+1)
-    left = (x-1, y)
-    right = (x+1, y)
+    up = (x-1, y)
+    down = (x+1, y)
+    left = (x, y-1)
+    right = (x, y+1)
 
-    if mapData[up] != -1:
-        u = Node((x,y-1),mapData[up],stateNode)
+    if mapData[up] != -1 and closed[up] != 1:
+        u = Node((x-1,y),mapData[up],stateNode)
         fringe.put(u)
+        print(Node.get_coordinates(u),"u")
 
-    elif mapData[down] != -1:
-        d = Node((x,y+1),mapData[down],stateNode)
+    if mapData[down] != -1 and closed[down] != 1:
+        d = Node((x+1,y),mapData[down],stateNode)
         fringe.put(d)
+        print(Node.get_coordinates(d),"d")
 
-    elif mapData[left] != -1:
-        l = Node((x-1,y),mapData[left],stateNode)
+    if mapData[left] != -1 and closed[left] != 1:
+        l = Node((x,y-1),mapData[left],stateNode)
         fringe.put(l)
-    
-    elif mapData[right] != -1:
-        r = Node((x+1,y),mapData[r],stateNode)
-        fringe.put(r)
+        print(Node.get_coordinates(l),"l")
 
+    if mapData[right] != -1 and closed[right] != 1:
+        r = Node((x,y+1),mapData[right],stateNode)
+        fringe.put(r)
+        print(Node.get_coordinates(r),"r")
+        
     return fringe
 
 #implementation of the graph search pseudocode
 def graphSearch(startPoint, endPoint, mapData, mapSize):
     
+    print
     startNode = initStartNode(startPoint,mapData)
-    closed = initClosed(mapSize, startNode)
+    closed = initClosed(mapSize)
     fringe = initFringe(startNode)
 
+    print(mapData)
     #run until fringe is empty or return triggered
     while fringe:          
         
@@ -125,19 +131,42 @@ def graphSearch(startPoint, endPoint, mapData, mapSize):
             return print("null")
         
         stateNode = fringe.get()
+        print(Node.get_coordinates(stateNode), "state node")
 
         if endCheck(stateNode, endPoint) == True:
             return stateNode
         
-        if closed[stateNode.get_coordinates] != 1:
-            closed[stateNode.get_coordinates] = 1
+        if closed[Node.get_coordinates(stateNode)] != 1:
+            closed[Node.get_coordinates(stateNode)] = 1
             expandFringe(stateNode,mapData,fringe,closed)
 
-#run function
+def outputMap(endNode, mapData, mapSize):
 
+    outputArray = mapData
+    while Node.get_parent(endNode) != None:
+    
+        coords = Node.get_coordinates(endNode)
+        outputArray[coords] = -2
+        endNode = Node.get_parent(endNode)
+    
+    outputArray[Node.get_coordinates(endNode)] = -2
+
+    a,b = mapSize
+
+    for x in range(1,a+1):
+        for y in range(1,b+1):
+            if outputArray[x,y] == -2:
+                print("*",end=" ")
+            elif outputArray[x,y] == -1:
+                print("X",end=" ")
+            else:
+                print(int(outputArray[x,y]),end=" ")
+        print()
+#run function
 mapSize,startPoint,endPoint,mapData,algorithm, heuristic = readFile()
 
-a = graphSearch(startPoint,endPoint,mapData,mapSize)
+endNode = graphSearch(startPoint,endPoint,mapData,mapSize)
 
-print(a.get_coordinates)
-print(a.get_elevation)
+outputMap(endNode,mapData,mapSize)
+
+
